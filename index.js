8bdaec59a5879b31569c21843bc31adc9564f030
Luiz Henrique Canet Filho <luiz.filho@infoglobo.com.br>
2019-02-26 18:47:30 -0300

    Creating Front-end structure with templating and assets

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var express = require('express')
 var app = express()
+var nunjucks = require('nunjucks')
 var LuckyHappyNumber = require('./lib/lucky-happy-number')
+
+app.locals.title = 'Lucky & Happy Number!'
+
+nunjucks.configure('templates', {
+  autoescape: true,
+  express: app
+})
+
+app.use('/assets', express.static('assets'))
 
 app.get('/lucky-happy/:number', (req, res) => {
   let number = Number(req.params.number)
@@ -10,6 +20,10 @@ app.get('/lucky-happy/:number', (req, res) => {
     isLucky: LuckyHappyNumber.isLucky(number),
     isHappy: LuckyHappyNumber.isHappy(number)
   })
+})
+
+app.get('/', (req, res) => {
+  res.render('index.html', app.locals)
 })
 
 app.listen(8080, () => {
