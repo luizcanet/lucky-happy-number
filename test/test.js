ada30e138d4b48a3cccb3bba85948904f3bc7da0
Luiz Henrique Canet Filho <me@luizca.net>
2019-02-26 02:05:34 -0300

    Lucky and Happy Number Library

--- test/test.js
+++ test/test.js
@@ -1,25 +1,69 @@
 /* global describe, it */
 var expect = require('chai').expect
-var luckyHappyNumber = require('lucky-happy-number')
+var LuckyHappyNumber = require('../src/lucky-happy-number')
 
 describe('Lucky Happy Number Library', function () {
+  describe('#getDigitCount()', function () {
+    it('Should return 1 when the value is 7', function () {
+      expect(LuckyHappyNumber.getDigitCount(7)).to.equal(1)
+    })
+
+    it('Should return 2 when the value is 21', function () {
+      expect(LuckyHappyNumber.getDigitCount(21)).to.equal(2)
+    })
+  })
+
+  describe('#getDigit()', function () {
+    it('Should return 6 when the values are 654 and 1', function () {
+      expect(LuckyHappyNumber.getDigit(654, 1)).to.equal(6)
+    })
+
+    it('Should return 5 when the value are 654 and 2', function () {
+      expect(LuckyHappyNumber.getDigit(654, 2)).to.equal(5)
+    })
+
+    it('Should return 4 when the value are 654 and 3', function () {
+      expect(LuckyHappyNumber.getDigit(654, 3)).to.equal(4)
+    })
+  })
+
   describe('#isHappy()', function () {
     it('Should return TRUE when the value is 7', function () {
-      expect(luckyHappyNumber.isHappy(7)).to.equal(true)
+      expect(LuckyHappyNumber.isHappy(7)).to.equal(true)
     })
 
     it('Should return FALSE when the value is 21', function () {
-      expect(luckyHappyNumber.isHappy(21)).to.equal(false)
+      expect(LuckyHappyNumber.isHappy(21)).to.equal(false)
     })
   })
 
   describe('#isLucky()', function () {
     it('Should return TRUE when the value is 7', function () {
-      expect(luckyHappyNumber.isLucky(7)).to.equal(true)
+      expect(LuckyHappyNumber.isLucky(7)).to.equal(true)
+    })
+
+    it('Should return TRUE when the value is 21', function () {
+      expect(LuckyHappyNumber.isLucky(21)).to.equal(true)
+    })
+
+    it('Should return TRUE when the value is 37', function () {
+      expect(LuckyHappyNumber.isLucky(37)).to.equal(true)
+    })
+
+    it('Should return FALSE when the value is 5', function () {
+      expect(LuckyHappyNumber.isLucky(5)).to.equal(false)
     })
 
     it('Should return FALSE when the value is 28', function () {
-      expect(luckyHappyNumber.isLucky(28)).to.equal(false)
+      expect(LuckyHappyNumber.isLucky(28)).to.equal(false)
+    })
+
+    it('Should return FALSE when the value is 142', function () {
+      expect(LuckyHappyNumber.isLucky(142)).to.equal(false)
+    })
+
+    it('Should return FALSE when the value is 100', function () {
+      expect(LuckyHappyNumber.isLucky(100)).to.equal(false)
     })
   })
 })
